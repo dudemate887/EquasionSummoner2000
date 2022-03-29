8578c010439bed403c5ee2c46a916b3c34703180
dudemate887 <gabrieliusdieninis@gmail.com>
2022-03-29 18:00:33 +0300

    Initial code from before

--- EquasionSummoner2000/Source.cpp
+++ EquasionSummoner2000/Source.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
+#include "Windows.h"
 using namespace std;
 
 int main()
 {
+    double x1, x2, y1, y2;
+    double rightFrac;
+
+    cin >> x1 >> y1 >> x2 >> y2;
+
+    rightFrac = (y2 - y1) / (x2 - x1);
+
+    cout << "y = " << rightFrac << "(x - " << x1 << ") + " << y1 << endl;
+
+    system("PAUSE");
 
 
-	return 0;
+    return 0;
 }
