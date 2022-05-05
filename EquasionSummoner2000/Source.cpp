3028a38d66006509b78f08981ceef81e98cccf07
dudemate887 <gabrieliusdieninis@gmail.com>
2022-05-05 17:30:28 +0300

    holy shit it actually works

--- EquasionSummoner2000/Source.cpp
+++ EquasionSummoner2000/Source.cpp
@@ -2,6 +2,13 @@
 #include "Windows.h"
 using namespace std;
 
+//Master equasion:
+    //cout << "y = " << rightFrac << "(x - " << x1 << ") + " << y1 << endl;
+//rightFrac omitted:
+    //cout << "y = (x - " << x1 << ") + " << y1 << endl;
+//y1 omitted:
+    //cout << "y = " << rightFrac << "(x - " << x1 << ")" << endl;
+
 int main()
 {
     double x1, x2, y1, y2;
@@ -9,9 +16,19 @@ int main()
 
     cin >> x1 >> y1 >> x2 >> y2;
 
-    rightFrac = (y2 - y1) / (x2 - x1);
+    rightFrac = (y2 - y1) / (x2 - x1); 
 
-    cout << "y = " << rightFrac << "(x - " << x1 << ") + " << y1 << endl;
+    cout << "y = ";
+    if (rightFrac != 1) cout << rightFrac;
+
+    if (x1 < 0) cout << "(x + " << abs(x1) << ")";
+    else if (x1 == 0) cout << "x";
+    else cout << "(x - " << x1 << ")";
+
+    if (y1 == 0) cout << " " << endl;
+    else if (y1 < 0) cout << " - " << y1 << endl;
+    else cout << " + " << y1 << endl;
+
 
     system("PAUSE");
 
